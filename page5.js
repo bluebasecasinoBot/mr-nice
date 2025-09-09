4160c4f3fba4af0d4c7dd6f6714596edb3e65d86
bluebasecasinoBot <developerme33@gmail.com>
2025-09-09 13:50:58 +0100

    Update page5.js

--- page5.js
+++ page5.js
@@ -27,13 +27,13 @@ __SYD.page5 = function()
 __SYD.p5_text = () =>{
     return $('p',{style:`height:fit-content;border-right:5px solid #000000;border-bottom:5px solid #000000;font-size:${__p(['page5' , '__font'],{2:'20px'})['2']};line-height: unset;width:100%;max-width:800px;text-align:center;font-weight:100;padding:${__p(["page5" , "__flex"],"row") === "row" ? "50px" : "20px"};background:#9945FF;border-radius:30px;color:#000000;display:flex;flex-direction:column;gap:15px;`},[
         $("p" , {style:`text-align:left;font-size:16px;`} , [
-            `That’s the story. $NICE is here, and it’s only getting bigger. We’re not just another coin — we’re a whole mood.`
+            `That’s the story. $xingsworld is here, and it’s only getting bigger. We’re not just another coin — we’re a whole mood.`
         ]),
         $("p" , {style:`text-align:left;font-size:16px;`} , [
-            `So hop in, grab some $NICE, share the memes, and let’s ride this wave together.`
+            `So hop in, grab some $xingsworld, share the memes, and let’s ride this wave together.`
         ]),
         $("p" , {style:`text-align:left;font-size:16px;`} , [
-            `Be nice. Stay nice. Stack $NICE.`
+            `Be nice. Stay nice. Stack $xingsworld.`
         ]),
 
         $(
